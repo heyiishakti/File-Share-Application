13da024e2083d42af0a8e8da6bea48555ecdfdcd
heyiishakti <heyiishakti@gmail.com>
2022-02-26 03:43:23 +0530

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const path = require('path');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
@@ -14,6 +15,16 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
+
+    //Cors
+
+const corsOptions = {
+        origin: process.env.ALLOWED.CLIENTS.split('.')
+}
+app.use(cors(corsOptions));
+
+
+//Template engine
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
