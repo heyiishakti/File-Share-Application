3c125addc15fbafc8df8443582f0b80b82ac8c53
heyiishakti <heyiishakti@gmail.com>
2022-03-15 01:47:30 +0530

    last commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -28,6 +28,10 @@ app.use(cors(corsOptions));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+// app.get('', (req, res) => {
+//     res.render ('download')
+// })
 
 //routes
 
